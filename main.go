8deb0d73dbfc3d890b4e6f35e488d3fb19eff210
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-09-05 12:42:25 +0200

    Hello Lambdas !

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// HelloWorldEvent : incoming event
+type HelloWorldEvent struct {
+	Name string `json:"name"`
+}
+
+// HandleRequest : handle the incoming requests
+func HandleRequest(ctx context.Context, evt HelloWorldEvent) (string, error) {
+	return fmt.Sprintf("Hello %s!", evt.Name), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
