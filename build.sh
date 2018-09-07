d0a9706be2f2d16cd50a4152507b625680307294
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-09-07 09:10:15 +0200

    Add the dist/ subfolder to the .zip

--- build.sh
+++ build.sh
@@ -9,7 +9,9 @@ esac
 
 echo "building for $GOOS-$GOARCH on $machine"
 
-for lambda in `ls lambdas`; do
+for lambda in `ls lambdas`
+do
+    echo ""
     echo "building $lambda"
     go build -o bin/$lambda lambdas/$lambda/main.go
     
@@ -21,6 +23,13 @@ for lambda in `ls lambdas`; do
     else
         zip -j dist/$lambda.zip bin/$lambda
     fi
+
+    # Add the /dist subfolder to the .zip if it exists
+    [ -e lambdas/$lambda/dist ] && \
+    echo "adding dist/" && \
+    cd lambdas/$lambda && \
+    zip -ru ../../dist/$lambda.zip dist && \
+    cd ../..
 
     echo "built dist/$lambda.zip"
 done
