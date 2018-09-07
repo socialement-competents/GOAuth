2223f828b7e15133f97fba808478471dab8037f7
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-09-07 13:26:57 +0200

    Fix hellolambda to work with an API Gateway Proxy

--- lambdas/hellolambda/main.go
+++ lambdas/hellolambda/main.go
@@ -4,17 +4,23 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// HelloWorldEvent : incoming event
-type HelloWorldEvent struct {
+// NameEvent : payload
+type NameEvent struct {
 	Name string `json:"name"`
 }
 
 // HandleRequest : handle the incoming requests
-func HandleRequest(ctx context.Context, evt HelloWorldEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", evt.Name), nil
+func HandleRequest(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	msg := fmt.Sprintf("Hello %s!", req.QueryStringParameters["name"])
+	resp := events.APIGatewayProxyResponse{
+		Body:       msg,
+		StatusCode: 200,
+	}
+	return resp, nil
 }
 
 func main() {
